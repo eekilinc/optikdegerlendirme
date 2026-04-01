6949672295b62d9317c8fe6842624ea1a0820d32
eekilinc <ekilinc@mehmetakif.edu.tr>
2026-04-01 19:30:24 +0300

    update

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,3 +6,21 @@ using System.Windows;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
+
+// Assembly attributes
+[assembly: AssemblyTitle("Optik Değerlendirme")]
+[assembly: AssemblyDescription("AĞLASUN MYO sınav değerlendirme sistemi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AĞLASUN MYO")]
+[assembly: AssemblyProduct("Optik Değerlendirme")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+// Target platform
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows")]
